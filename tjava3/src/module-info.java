46f76dc17361b5eaecf234a97d26069090ff2744
tezerabe <88900356+tezerabe@users.noreply.github.com>
2021-08-20 17:48:10 -0700

    commit

---
+++ tjava3/src/module-info.java
@@ -0,0 +1,2 @@
+module tjava3 {
+}
---
+++ tjava3/src/tjava3/tjava3class.java
@@ -0,0 +1,11 @@
+package tjava3;
+
+public class tjava3class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+
+	}
+
+}
